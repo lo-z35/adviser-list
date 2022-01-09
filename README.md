ad80337f18c950ffc34bb913bb51b01d645b310d
Lukas Ortwein <lukas.ortwein@zone35.de>
2022-01-09 18:03:09 +0100

    instructions

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# Getting Started with Create React App
+# Anweisungen
+
+Die Anwendung besteht aus zwei Views:
+
+1.) Beraterliste:
+
+In der Datei mockdata.js befindet sich eine Array aus Usern (Beratern).
+
+Alle Berater der Array, deren "active"-Key true ist, sollen hier in einer Liste dargestellt werden. Dabei sollen "profile_picture" als runder Avatar, "username" als Überschrift, "beratungsfelder" als ungeordnete Liste sowie die "beschreibung" des jeweiligen Beraters ausgewiesen werden.
+
+Das folgende Desgin soll bei der Erstellung der Komponenten umgesetzt werden:
 
 ![userlist](./userlist.png)
 
+Unter jedem Berater in der Liste befindet sich ein Button ("Berater\*in auswählen"). Ein Klick auf diesen Button öffnet das Profil des Beraters und führt zum zweiten View:
+
+2.) Profil des ausgewählten Beraters:
+
+Hier wird nur der Berater, der ausgewählt wurde dargestellt mit einem größeren Avatar. Dabei soll auch sein Onlinestatus ("online_status") ausgewiesen werden (Schriftfarbe grün für online und rot für offline). Ein Klick auf den Button "Berater\*in wechseln" führt zurück zur Beraterliste. Der Button "Beratung starten" hat keine Funktion.
+
+Designvorlage:
+
 ![profile](./profile.png)
+
+Bonus:
+
+1.) Suche:
+Erstelle einen Input über der Beraterliste, mit dem man nach dem username des Beraters suchen kann. Beispiel: Eingabe von "Ma" führt dazu, dass nur die Berater "Max Mustermann" und "Marianne Wagner" dargestellt werden.
+
+2.) Routing:
+Erstelle jeweils eine Route für die Beraterliste ("/beraterliste") und das Profil des Beraters, bei dem die Id des Beraters als param verwendet wird ("berater/{id}").
---
+++ mockdata.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    username: "Max Mustermann",
+    beratungsfelder: ["Beziehung", "Sucht"],
+    beschreibung:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    online_status: "offline",
+    active: true,
+  },
+  {
+    id: 2,
+    username: "Marianne Wagner",
+    beratungsfelder: ["Schwangerschaft", "Beziehung"],
+    beschreibung:
+      "Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+    online_status: "online",
+    active: true,
+  },
+  {
+    id: 3,
+    username: "Peter Müller",
+    beratungsfelder: ["Paartherapie", "Essstörungen", "Alkohol"],
+    beschreibung:
+      "At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    online_status: "online",
+    active: true,
+  },
+  {
+    id: 4,
+    username: "Jürgen Schmidt",
+    beratungsfelder: ["Beziehung"],
+    beschreibung:
+      "Justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    online_status: "online",
+    active: false,
+  },
+];
